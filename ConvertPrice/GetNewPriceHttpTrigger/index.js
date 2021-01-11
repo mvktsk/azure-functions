a8b86ef423a60233b8b2ed00fe7402908689b26b
Maksim Kopnov <mko@virtoway.com>
2021-01-11 17:41:59 +0700

    Refactoring

--- ConvertPrice/GetNewPriceHttpTrigger/index.js
+++ ConvertPrice/GetNewPriceHttpTrigger/index.js
@@ -1,37 +1,67 @@
 const fetch = require("node-fetch");
 
 const getData = async url => {
-  try {
-    const response = await fetch(url);
-    const json = await response.json();
-    console.log(json);
-    return json;
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        context.log('getData function processed a request.');
+        const response = await fetch(url);
+        const json = await response.json();
+        console.log(json);
+        return json;
+    }
+    catch (error) {
+        console.log(error);
+    }
 };
 
 
 module.exports = async function (context, req) {
 
-    const baseCurrency = req.query.base;
-    const requiredCurrency = req.query.required;
-    const currencyQuery = "https://api.exchangeratesapi.io/latest?symbols=" + requiredCurrency + "&base=" + baseCurrency;
+    // Input parameters 
+    // base - base (current) item price currency (convert from); default value USD;
+    // required - required item currency (convert to); default value EUR;
+    // price - item price;
+    // rate - price conversation rate (if you need to add some discount after conversation rate should be < 1, 
+    // for markup, rate should be > 1, for "as is" conversation use rate = 1); default value 1.
+
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    var responseBody;
+    var responseStatus;
+
+    const baseCurrency = req.query.base ? req.query.base : 'USD';
+    const requiredCurrency = req.query.required ? req.query.required : 'EUR';
+    const price = req.query.price;
+    const rate = req.query.rate ? req.query.rate : 1;
+
+    const currencyQuery = `https://api.exchangeratesapi.io/latest?symbols=${requiredCurrency}&base=${baseCurrency}`;
+    console.log(currencyQuery);
 
     const currencyResponse = await getData(currencyQuery);
 
-    const price = req.query.price;
-    newPrice = price *  currencyResponse.rates[requiredCurrency];
-    const responseMessage = currencyResponse 
-        ? "Currency Query " + currencyQuery + "\n" +
-        "Currency response " + currencyResponse  + "\n" +
-        "Price " + price  + "\n" +
-        "New price " + newPrice
-        : "No currency response.";
+    if (currencyResponse && price){
+
+        const currencyRate = currencyResponse.rates[requiredCurrency] ? currencyResponse.rates[requiredCurrency] : 1;
+        const convertedPrice = price * currencyRate * rate;
+
+        responseBody = {
+            baseCurrency: baseCurrency,
+            requiredCurrency: requiredCurrency,
+            currencyRate: currencyRate,
+            price: price,
+            convertedPrice: convertedPrice,
+            rate: rate
+        }
+        responseStatus = 200; // Ok
+    }
+    else{
+        responseStatus = 400; // Bad Request 
+        responseBody = '';
+        if (!price) responseBody = 'Required input parameter price are not defined \n';
+        if (!currencyResponse) responseBody = 'api.exchangeratesapi.io not response \n';
+    }
 
     context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: responseMessage
+        status: responseStatus, 
+        body: responseBody
     };
-
 }
